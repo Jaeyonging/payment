d475b160adb87ed3f35a4d54c57f1c1c5fa4481c
jaeyonging <wodyd1318@naver.com>
2024-05-29 09:33:57 +0900

    init

---
+++ src/routes/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='flex w-[100vw] bg-[red] text-[yellow] h-[100vh] justify-center items-center'>
+      <div>
+        Testing tailwind
+      </div>
+    </div >
+  )
+}
---
+++ src/types/type.ts
@@ -0,0 +1,14 @@
+export interface QTest {
+    question: string
+    answer01: string
+    answer02: string
+    answer03: string
+    ansewr04: string
+    answer05: string
+}
+
+
+export const QMap: Map<string, string> = new Map([
+
+
+]);
