c1f8615d547a62ffed172e3277b50f7a88a66d49
jaeyonging <wodyd1318@naver.com>
2024-05-29 10:52:02 +0900

    fix: menu and home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./routes/Home";
+import { Menu } from "./routes/Menu";
 import './App.css'
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <Suspense fallback={<div>로딩중</div>}>
       <Routes>
         <Route path="/" element={<Home></Home>} />
+        <Route path="/menu" element={<Menu></Menu>} />
         <Route path="/detail/:id" element={<Home></Home>} />
-        <Route path="/cart" element={<Home></Home>}/>
+        <Route path="/cart" element={<Home></Home>} />
       </Routes>
     </Suspense>
   );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import Slider from 'react-slick';
+import '../styles/slick-theme.css'
+import '../styles/slick.css'
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
+  const navigate = useNavigate()
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+
+  const onclick = () => {
+    console.log("click")
+    navigate("/menu")
+  }
+
   return (
-    <div className='flex w-[100vw] bg-[red] text-[yellow] h-[100vh] justify-center items-center'>
-      <div>
-        Testing tailwind
-      </div>
+    <div>
+      < div onClick={onclick}>
+        <Slider {...settings} className='absolute w-[100vw] h-[100vh]'>
+          <img src="https://cdn.bonif.co.kr/cmdt/20220628_qRJ_1656371947942_631Kb.jpg" className='w-[100vw] h-[100vh]' />
+          <img src="https://cdn.bonif.co.kr/cmdt/20220628_qRJ_1656371947942_631Kb.jpg" className='w-[100vw] h-[100vh]' />
+        </Slider>
+      </div >
     </div >
   )
 }
